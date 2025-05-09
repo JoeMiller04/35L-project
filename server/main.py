c7e62dd2432f3875c8f18291c64a0a055d60e2e4
JoeMiller04 <josephmiller314159@gmail.com>
2025-05-09 14:36:10 -0700

    Allow Any Origin

--- server/main.py
+++ server/main.py
@@ -7,11 +7,12 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000"
+
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
