6e111d04d63085d54360e9be0bd9a90becb1e95a
JoeMiller04 <josephmiller314159@gmail.com>
2025-05-30 14:15:23 -0700

    Update Course List Checks
    Course list will now prevent you from adding classes if they have time conflicts on the schedule.
    Updated tests to reflect changes

--- client/src/SearchPage.js
+++ client/src/SearchPage.js
@@ -8,12 +8,12 @@ function SearchPage() {
 
     return (
         <div>
-            <h1>Welcome to the Home2 Page</h1>
+            <h1>Welcome to the Joe's Page</h1>
             <p>This is the second page of our application.</p>
-            <button onClick={() => navigate('/Home')}>Go to Home</button>
-            <button onClick={() => navigate('/InfoPage')}>Go to Home3</button>
-            <button onClick={() => navigate('/PastCourses')}>Go to Home4</button>
-            <button onClick={() => navigate('/FuturePlanner')}>Go to Home5</button>
+            <button onClick={() => navigate('/Home')}>Go to Schedule Planner Page</button>
+            <button onClick={() => navigate('/InfoPage')}>Go to Empty Home3</button>
+            <button onClick={() => navigate('/PastCourses')}>CSE Course Checklist Page</button>
+            <button onClick={() => navigate('/FuturePlanner')}>Future Course Planner Page</button>
         </div>
     );
 }
